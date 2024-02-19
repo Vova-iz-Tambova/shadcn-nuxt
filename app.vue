783e534263e526bad2aa03db5c4e4991e1b53030
Vova iz Tambova <voller.ruden@yandex.ru>
2024-02-19 13:29:30 +0300

    add ui button

--- app.vue
+++ app.vue
@@ -1,5 +1,18 @@
+<script setup lang="ts">
+import { Mail, Loader2 } from "lucide-vue-next";
+import { Button } from "@/components/ui/button";
+</script>
+
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <Button>Primary</Button>
+  <Button variant="secondary">Secondary</Button>
+  <Button variant="destructive"> Destructive </Button>
+  <Button variant="outline"> Outline </Button>
+  <Button variant="ghost"> Ghost </Button>
+  <Button variant="link"> Link </Button>
+  <Button> <Mail class="w-4 h-4 mr-2" /> Login with Email </Button>
+  <Button disabled>
+    <Loader2 class="w-4 h-4 mr-2 animate-spin" />
+    Please wait
+  </Button>
 </template>
